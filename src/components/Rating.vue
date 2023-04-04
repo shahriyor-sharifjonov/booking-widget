092d6d631ecd97821d8511eb81c24129c5bcf0ab
shahriyor-sharifjonov <shahriyordeveloping@gmail.com>
2023-04-04 17:44:16 +0500

    init

---
+++ src/components/Rating.vue
@@ -0,0 +1,18 @@
+<script setup>
+import RatingItem from './RatingItem.vue'
+</script>
+
+<template>
+    <div class="book-order__rating">
+        <div class="book-order__rating-content">
+          <p class="book-order__rating-title">Our rating speak for themselves</p>
+          <p class="book-order__rating-desc">Over 6000 + travellers ❤️ us</p>
+        </div>
+        <div class="book-order__rating-row">
+          <RatingItem img="/facebook.svg" score="4.7"/>
+          <RatingItem img="/trip-advisor.svg" score="4.5"/>
+          <RatingItem img="/trustpilot.svg" score="4.6"/>
+          <RatingItem img="/google.svg" score="5"/>
+        </div>
+    </div>
+</template>
