305b261a5c63c0c3d003d62474d7f41f54d84984
shahriyor-sharifjonov <shahriyordeveloping@gmail.com>
2023-04-09 18:40:33 +0500

    initial

--- src/components/PersFlight.vue
+++ src/components/PersFlight.vue
@@ -15,7 +15,7 @@
                     <p class="book-pers__title book-pers__title_small">Flight number according to the airline ticket *</p>
                 </div>
             </div>
-        </div>
+        </div> 
         <div class="book-pers__bot pl-0">
             <div class="book-pers__field">
                 <input class="book-pers__field-text" placeholder="E.g. DL9530" />
