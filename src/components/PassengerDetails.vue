47363406718441a128c9ecec7f24382c152c6262
shahriyor-sharifjonov <shahriyordeveloping@gmail.com>
2023-04-10 10:21:48 +0500

    init

--- src/components/PassengerDetails.vue
+++ src/components/PassengerDetails.vue
@@ -2,15 +2,15 @@
     <div class="book-order__cars">
         <p class="book-order__cars-title">Passenger Details</p>
         <div class="book-details__field">
-            <p class="book-details__field-label">Full name*</p>
+            <p class="book-details__field-label">Full name *</p>
             <input type="text" class="book-details__field-input" />
         </div>
         <div class="book-details__field">
-            <p class="book-details__field-label">Email*</p>
+            <p class="book-details__field-label">Email *</p>
             <input type="email" class="book-details__field-input" />
         </div>
         <div class="book-details__field">
-            <p class="book-details__field-label">Phone number*</p>
+            <p class="book-details__field-label">Phone number *</p>
             <input type="tel" class="book-details__field-input" />
         </div>
         <p class="book-details__p">
