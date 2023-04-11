d4ee31548bae218db658c4614896bd26587e7d3e
shahriyor-sharifjonov <shahriyordeveloping@gmail.com>
2023-04-11 16:47:02 +0500

    fix bugs

--- src/components/PersWait.vue
+++ src/components/PersWait.vue
@@ -2,7 +2,7 @@
     export default {
         data() {
             return {
-                open: false
+                open: false,
             }
         },
     }
@@ -15,14 +15,19 @@
                 <img src="/time.svg" alt="" draggable="false" />
                 <div class="book-pers__top-content">
                     <p class="book-pers__title">Extra waiting time</p>
-                    <p class="book-pers__p book-pers__p_green">Instead of 1 hour the drivers wait 2 hours at the airport, an additional fee of 30.00€ applies</p>
+                    <p class="book-pers__p book-pers__p_green">For every hour, an additional fee of 30.00€ applies</p>
                 </div>
             </div>
             <div class="book-widget__checkbox-input">
                 <input type="checkbox" v-model="open" />
                 <div class="book-widget__checkbox-overlay">
-                <div></div>
+                    <div></div>
                 </div>
+            </div>
+        </div>
+        <div class="book-pers__bot" v-if="open">
+            <div class="book-pers__field">
+                <input type="number" min="1" max="5" class="book-pers__field-text"/>
             </div>
         </div>
     </div>
