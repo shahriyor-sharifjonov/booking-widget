517b4eaf825a56240af63053a78b498dbfb78f3b
shahriyor-sharifjonov <shahriyordeveloping@gmail.com>
2023-04-12 14:13:11 +0500

    initial

--- src/components/PersSign.vue
+++ src/components/PersSign.vue
@@ -1,5 +1,50 @@
 <script>
+    import {ref} from 'vue'
+    import { PlusOutlined } from '@ant-design/icons-vue';
+    // import { UploadProps } from 'ant-design-vue';
+
+    function getBase64(file) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => resolve(reader.result);
+            reader.onerror = error => reject(error);
+        });
+    }
+
     export default {
+        components: {
+            PlusOutlined,
+        },
+        setup() {
+            const previewVisible = ref(false);
+            const previewImage = ref('');
+            const previewTitle = ref('');
+
+            const fileList = ref([]);
+
+            const handleCancel = () => {
+                previewVisible.value = false;
+                previewTitle.value = '';
+            };
+            const handlePreview = async (file) => {
+                if (!file.url && !file.preview) {
+                    file.preview = (await getBase64(file.originFileObj));
+                }
+                previewImage.value = file.url || file.preview;
+                previewVisible.value = true;
+                previewTitle.value = file.name || file.url.substring(file.url.lastIndexOf('/') + 1);
+            };
+
+            return {
+                previewVisible,
+                previewImage,
+                fileList,
+                handleCancel,
+                handlePreview,
+                previewTitle,
+            };
+        },
         data() {
             return {
                 open: false
@@ -28,8 +73,40 @@
         <div class="book-pers__bot" v-if="open">
             <div class="book-pers__field">
                 <input type="text" class="book-pers__field-text" placeholder="Name on the sign" />
-                <input type="file" class="book-pers__field-file" />
+                <div class="clearfix">
+                    <a-upload
+                    v-model:file-list="fileList"
+                    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                    list-type="picture-card"
+                    @preview="handlePreview"
+                    >
+                    <div v-if="fileList.length < 1">
+                        <plus-outlined />
+                        <div style="margin-top: 8px">Upload</div>
+                    </div>
+                    </a-upload>
+                    <a-modal :visible="previewVisible" :title="previewTitle" :footer="null" @cancel="handleCancel">
+                    <img alt="example" style="width: 100%" :src="previewImage" />
+                    </a-modal>
+                </div>
             </div>
         </div>
     </div>
 </template>
+
+<style>
+    .ant-upload-select-picture-card i {
+        font-size: 32px;
+        color: #999;
+    }
+
+    .ant-upload-select-picture-card .ant-upload-text {
+        /* margin-top: 8px; */
+        color: #666;
+    }
+
+    .clearfix{
+        margin-top: 15px;
+        min-height: 112px;
+    }
+</style>
